2480fec7da1b30dcde7e16ceb6259cc6be9307ec
Al Astudillo <alastudillo@renesys.es>
2024-09-24 10:44:25 +0200

    Cleanup

--- lifevision.cpp
+++ lifevision.cpp
@@ -17,7 +17,7 @@ cv::Mat GoL(cv::Mat input, int iters){
 
         for (int i = 0; i < input.rows; i++) {
             for (int j = 0; j < input.cols; j++) {
-                uchar &pixel = input.at<uchar>(i, j);  // For 3-channel (BGR) images
+                uchar &pixel = input.at<uchar>(i, j); 
                 bool alive = pixel>(int)0;
                 int num_alive_neighbours = 0;
                 int neighbours_mean = 0;
@@ -54,8 +54,8 @@ cv::Mat GoL(cv::Mat input, int iters){
 }
 
 int main() {
-    cv::VideoCapture cap(0); // Open the default camera
-    if (!cap.isOpened()) { // Check if we succeeded
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
         std::cerr << "Could not open camera" << std::endl;
         return -1;
     }
@@ -66,28 +66,26 @@ int main() {
     double scale_factor = 1;  // Scaling down to 25%
     cv::resize(frame, frame, cv::Size(), scale_factor, scale_factor);
     cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-    cv::Laplacian(gray, edges, CV_16S, 3);  // Apply Laplacian with kernel size 3
-    cv::convertScaleAbs(edges, edges);      // Convert back to 8-bit format
+    cv::Laplacian(gray, edges, CV_16S, 3);
+    cv::convertScaleAbs(edges, edges);
     cv::threshold(gray, old_frame, 156, 255, cv::THRESH_BINARY_INV);
 
     while (true) {
         cap >> frame;
 
         if (frame.empty()) {
-            break; // End of video stream
+            break;
         }
 
         cv::resize(frame, frame, cv::Size(), scale_factor, scale_factor);
-        cv::flip(frame, dst, 1);     // because you can't flip in-place (leads to segfault)
+        cv::flip(frame, dst, 1);
         cv::cvtColor(dst, gray, cv::COLOR_BGR2GRAY);
-            // Apply the Laplacian edge detection
         cv::Canny(gray, edges, 100, 200);
-        //cv::convertScaleAbs(edges, edges);      // Convert back to 8-bit format
+        //cv::convertScaleAbs(edges, edges);
 
         cv::threshold(edges, binary, 1, 255, cv::THRESH_BINARY);
         cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 1));
 
-        // Apply dilation to make the edges thicker
         cv::dilate(binary, thick_edges, kernel);
         //cv::bitwise_not(thick_edges, thick_edges); 
         cv::Mat combined;
@@ -95,11 +93,11 @@ int main() {
         cv::bitwise_or(thick_edges, old_frame, combined);
         //combined = binary;
          for(int n = 0; n<1;n++){
-            //if (cv::waitKey(30) >= 0) break; // Wait for a keystroke in the window or 30ms
+            //if (cv::waitKey(30) >= 0) break; 
             old_frame = GoL(combined,1);
             //old_frame = edges;
             //cv::Mat upscaled;
-            //cv::resize(old_frame, upscaled, frame.size());  // Resize back to the original size
+            //cv::resize(old_frame, upscaled, frame.size()); 
             if(first){
                 first = false;
                 break;
@@ -111,10 +109,10 @@ int main() {
         }
 
         //cv::imshow("Webcam", old_frame);
-        if (cv::waitKey(10) >= 0) break; // Wait for a keystroke in the window or 30ms
+        if (cv::waitKey(10) >= 0) break; 
     }
 
-    cap.release(); // Release the VideoCapture object
-    cv::destroyAllWindows(); // Close all OpenCV windows
+    cap.release(); 
+    cv::destroyAllWindows();
     return 0;
 }
